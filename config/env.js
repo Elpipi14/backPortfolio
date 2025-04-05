1feefa2a257b206d8ce2efd425233cbb95f767c5
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2025-04-05 05:03:36 -0300

    porfolio

--- config/env.js
+++ config/env.js
@@ -5,9 +5,9 @@ const configObject = {
   email_user: process.env.EMAIL_USER,
   email_pass: process.env.EMAIL_PASS,
   RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
-  page: process.env.PAGE || "http://localhost:8080",
-  page2: process.env.PAGE2 || "https://localhost:8080",
-  page3: process.env.PAGE3 || "https://localhost:8080",
+  page: process.env.PAGE,
+  page2: process.env.PAGE2,
+  page3: process.env.PAGE3,
   port: process.env.PORT || 8080,
 };
 
