09a34b404a7fc405b23a92b418941f64701c4858
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2025-04-05 20:25:58 -0300

    portfolio

--- middlewares/validation.js
+++
@@ -1,9 +0,0 @@
-import { check } from "express-validator";
-
-export const validateContact = [
-  check("firstName").notEmpty().withMessage("Nombre requerido"),
-  check("lastName").notEmpty().withMessage("Apellido requerido"),
-  check("email").isEmail().withMessage("Email inválido"),
-  check("message").isLength({ min: 10 }).withMessage("Mensaje muy corto"),
-];
-
