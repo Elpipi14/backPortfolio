778be5ded9e1af46b79e2a3fcaa6f4a86f55f7af
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2025-04-05 01:18:22 -0300

    porfolio

--- routes/contact.js
+++ routes/contact.js
@@ -8,25 +8,25 @@ const { RECAPTCHA_SECRET_KEY } = configObject;
 
 
 routerContact.post("/api/contact", async (req, res) => {
-    const { firstName, lastName, email, message, token } = req.body;
-  
-    if (!firstName || !lastName || !email || !message || !token) {
+    const { firstName, lastName, email, message} = req.body;
+  // , token || !token
+    if (!firstName || !lastName || !email || !message ) {
       return res.status(400).json({ success: false, error: "Datos incompletos" });
     }
   
     try {
       // ✅ Verificamos el token de reCAPTCHA con Google
-      const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
-      const captchaRes = await axios.post(verifyUrl, null, {
-        params: {
-          secret: RECAPTCHA_SECRET_KEY,
-          response: token,
-        },
-      });
+      // const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
+      // const captchaRes = await axios.post(verifyUrl, null, {
+      //   params: {
+      //     secret: RECAPTCHA_SECRET_KEY,
+      //     response: token,
+      //   },
+      // });
   
-      if (!captchaRes.data.success) {
-        return res.status(400).json({ success: false, error: "Captcha inválido" });
-      }
+      // if (!captchaRes.data.success) {
+      //   return res.status(400).json({ success: false, error: "Captcha inválido" });
+      // }
   
       // ✅ Si pasó el captcha, mandamos el email
       await sendConfirmationEmail({ email, firstName });
