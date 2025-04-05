dd142b984629938b35c3197dfd86fe1e2df0fe36
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2025-04-05 19:34:57 -0300

    portfolio

--- routes/contact.js
+++ routes/contact.js
@@ -1,41 +1,45 @@
 import { Router } from "express";
 import axios from "axios";
-import configObject from '../config/env.js'
+import configObject from "../config/env.js";
 import { sendConfirmationEmail } from "../utils/nodemailer.js";
 
 const routerContact = Router();
 // const { RECAPTCHA_SECRET_KEY } = configObject;
 
-
 routerContact.post("/api/contact", async (req, res) => {
-    const { firstName, lastName, email, message} = req.body;
+  const { firstName, lastName, email, message } = req.body;
   // , token || !token
-    if (!firstName || !lastName || !email || !message ) {
-      return res.status(400).json({ success: false, error: "Datos incompletos" });
-    }
-  
-    try {
-      // ✅ Verificamos el token de reCAPTCHA con Google
-      // const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
-      // const captchaRes = await axios.post(verifyUrl, null, {
-      //   params: {
-      //     secret: RECAPTCHA_SECRET_KEY,
-      //     response: token,
-      //   },
-      // });
-  
-      // if (!captchaRes.data.success) {
-      //   return res.status(400).json({ success: false, error: "Captcha inválido" });
-      // }
-  
-      // ✅ Si pasó el captcha, mandamos el email
-      await sendConfirmationEmail({ email, firstName });
-  
-      res.json({ success: true, message: "Correo enviado con éxito." });
-    } catch (error) {
-      console.error("Error en el servidor:", error);
-      res.status(500).json({ success: false, error: "Error del servidor." });
-    }
-  });
+  if (!firstName || !lastName || !email || !message) {
+    return res.status(400).json({ success: false, error: "Datos incompletos" });
+  }
+
+  try {
+    // ✅ Verificamos el token de reCAPTCHA con Google
+    // const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
+    // const captchaRes = await axios.post(verifyUrl, null, {
+    //   params: {
+    //     secret: RECAPTCHA_SECRET_KEY,
+    //     response: token,
+    //   },
+    // });
+
+    // if (!captchaRes.data.success) {
+    //   return res.status(400).json({ success: false, error: "Captcha inválido" });
+    // }
+
+    // ✅ Si pasó el captcha, mandamos el email
+    await sendConfirmationEmail({
+      email,
+      firstName,
+      last_name: lastName,
+      message,
+    });
+
+    res.json({ success: true, message: "Correo enviado con éxito." });
+  } catch (error) {
+    console.error("Error en el servidor:", error);
+    res.status(500).json({ success: false, error: "Error del servidor." });
+  }
+});
 
 export default routerContact;
